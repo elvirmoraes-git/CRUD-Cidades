22a15bfc835ada787f5b28be1a205afed3687d39
elvirmoraes-git <elvir.moraes@outlook.com>
2023-05-07 17:18:02 -0300

    Criado método findByNomeAndEstado para retornar uma CidadeEntidade com os parâmetros representando o nome e o estado.

---
+++ src/main/java/br/edu/utfpr/cp/espjava/CRUDCidades/visao/CidadeEntidade.java
@@ -0,0 +1,38 @@
+package br.edu.utfpr.cp.espjava.CRUDCidades.visao;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity (name = "cidade")
+public class CidadeEntidade implements Serializable{
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String nome;
+    private String estado;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getEstado() {
+        return estado;
+    }
+}
---
+++ src/main/java/br/edu/utfpr/cp/espjava/CRUDCidades/visao/CidadeRepository.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.cp.espjava.CRUDCidades.visao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+
+
+public interface CidadeRepository extends JpaRepository<CidadeEntidade, Long>{
+
+    public Optional<CidadeEntidade> findByNomeAndEstado(String nome, String estado);
+}
